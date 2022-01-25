1b24e95a2fadae5df3687aaba0ad536de0d51a9f
coollama1 <lamemarvin@gmail.com>
2022-01-24 21:17:33 -0500

    initial commit

---
+++ src/test/java/com/projects/mynotepad/MyNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.mynotepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
